a5720c7b1e1f7c3bcdf0b9342aefebce63bf8faf
emenogu <uchen4all@gmail.com>
2022-09-07 09:33:49 -0700

    wildcard

--- wildcards.c
+++ wildcards.c
@@ -17,3 +17,60 @@ void expandWildcardsIfNecessary(char * arg)
 	char * a = arg;
 	char * r = reg;
 	*r = '^' ; r++; /* match beginning of line */
+	while(*a)
+	{
+		if(*a == '*')
+		{
+			*r = '.'; 
+			r++;
+			*r = '*';
+			r++;
+		}
+		else if(*a == '?')
+		{
+			*r = '.';
+			r++;
+		else if(*a == '.')
+		{
+			*r = '\\';
+			r++;
+			*r = '.';
+			r++;
+		}
+		else
+		{
+			*r = *a;
+			r++;
+		}
+		a++;
+	}
+	*r = '$';
+	r++;
+	*r = 0; /* match end of line and add null character */
+	/* compile regular expression */
+	char * expbuf = regcomp(reg, â€|);
+	if(expbuf == NULL)
+	{
+		perror("regcomp");
+		return;
+	}
+	/* 3. List directory and add ass arguments */
+	/* the entries that match the regular expression*/
+	DIR * dir = opendir(".");
+	if(dir == NULL)
+	{
+		perror("oopendir");
+		return;
+	}
+	struct dirent *ent;
+	while((ent = readdir(dir)) != NULL)
+	{
+		/* check if name matches */
+		if(regexec(ent ->d_name, re) == 0)
+		{
+			/* add argument*/
+			Command::_currentSimpleCommand ->insertArgument(strdup(ent ->d_name));
+		}
+	}
+	closedir(dir);
+}
