af661e6c49609100bec82a1a69e1587781aac516
emenogu <uchen4all@gmail.com>
2022-09-07 12:43:51 -0700

    command table

--- cmdtable.c
+++ cmdtable.c
@@ -13,28 +13,30 @@ struct simpleCommand
 	/* Number of arguments*/
 	int _numberOfArguments;
 	/* Array of arguments*/
-	char ** _arguments;
+	char **_arguments;
 
 	simpleCommand();
 	void insertArgument(char *argument);
 };
-/* describes a complete command with multiple pipes or input output redirections if any*/
-struct Command {
+/* describes a complete command with multiple pipes*/
+/* or input output redirections if any*/
+struct Command 
+{
 	int _numberOfAvailableSimpleCommands;
 	int _numberOfSimpleCommands;
-	simpleCommand ** _simpleCommands;
-	char * _outFile;
-	char * _inputFile;
-	char * _errFile;
+	simpleCommand **_simpleCommands;
+	char *_outFile;
+	char *_inputFile;
+	char *_errFile;
 	int _background;
 
-	void prompt();
-	void print();
-	void execute();
-	void clear();
+	void prompt(void);
+	void print(void);
+	void execute(void);
+	void clear(void);
 
 	Command();
-	void insertSimpleCommand(simpleCommand * SimpleCommand);
+	void insertSimpleCommand(simpleCommand *SimpleCommand);
 
 	static Command _currentCommand;
 	static simpleCommand *_currentSimpleCommand;
